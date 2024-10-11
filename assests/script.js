3afe8e483229bba8b7a72a980411b5d7e2f49812
elmira <elledevelope@gmail.com>
2024-10-11 10:02:56 +0200

    Added color change for text inside cube

--- assests/script.js
+++ assests/script.js
@@ -1,24 +1,49 @@
 let cube = document.querySelector("#cube");
+let red = document.querySelector(".red");
+let green = document.querySelector(".green");
+let blue = document.querySelector(".blue");
+let color = document.querySelector(".color");
 console.dir(cube);
-// R key: 82 red
-// G key: 71 green
-// B key: 66 blue
 
-function changeColor(color) {
-    cube.style.backgroundColor = color;
+
+// Function to change the cube color :
+function changeColor(colorCube) {
+    cube.style.backgroundColor = colorCube;
+}
+
+// Function to display the appropriate text based on color :
+function textDisplay(colorCube) {
+    // Text initially hidden
+    red.style.display = "none";
+    green.style.display = "none";
+    blue.style.display = "none";
+
+    if (colorCube === "red") {
+        red.style.display = "block";
+        red.style.color = "darkred";
+    } else if (colorCube === "green") {
+        green.style.display = "block";
+        green.style.color = "greenyellow";
+    } else if (colorCube === "blue") {
+        blue.style.display = "block";
+        blue.style.color = "darkblue";
+    }
 }
 
 window.addEventListener("keyup", function (e) {
     switch (e.keyCode) {
-        case 82: //---------------- red  
+        case 82: //---------------- R key: 82 red  
             changeColor("red");
+            textDisplay("red");
             break;
-        case 71: //---------------- green
+        case 71: //---------------- G key: 71 green
             changeColor("green");
+            textDisplay("green");
             break;
 
-        case 66: //---------------- blue
+        case 66: //---------------- B key: 66 blue
             changeColor("blue");
+            textDisplay("blue");
             break;
 
         default:
